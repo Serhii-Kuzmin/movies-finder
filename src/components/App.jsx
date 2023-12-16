1bf744565421e15f88393af182b89411dedd2fd7
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-17 00:41:55 +0100

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+// import MoviesDetails from "pages/MoviesDetails";
+// import Reviews from "./Reviews/Reviews";
+import { lazy } from 'react';
+
+const Home = lazy(() => import('../pages/Home'));
+const MoviesDetails = lazy(() => import('../pages/MoviesDetails'));
+const Movies = lazy(() => import('../pages/Movies'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="movies" element={<Movies />} />
+      <Route path="movies/:movieId" element={<MoviesDetails />}>
+        <Route path="cast" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<Home />} />
+    </Route>
+  </Routes>;
 };
