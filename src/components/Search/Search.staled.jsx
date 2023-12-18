dd091121bf4463ce37f6c7e13e4e6777a2d23962
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-18 18:34:53 +0100

    styled

--- src/components/Search/Search.staled.jsx
+++ src/components/Search/Search.staled.jsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const SearchbarStyle = styled.header
-  `top: 0;
+export const SearchbarStyle = styled.header`
+  top: 0;
   left: 0;
   position: sticky;
   z-index: 1100;
@@ -14,7 +14,7 @@ export const SearchbarStyle = styled.header
   padding-top: 12px;
   padding-bottom: 12px;
   color: #fff;
-  background-color: #3f51b5;
+  background-color: #b7cabc;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
@@ -25,25 +25,25 @@ export const SearchForm = styled.form`
   width: 100%;
   max-width: 600px;
   background-color: #fff;
-  border-radius: 3px;
+  border-radius: 5px;
   overflow: hidden;
 `;
 
 export const SearchFormButton = styled.button`
   display: inline-block;
-  /* width: 48px; */
-  height: 48px;
-  border: 0;
-  padding: 10px;
-  font: inherit;
-  font-weight: 500;
-  font-size: 20px;
-  opacity: 0.6;
-  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  padding: 10px 20px;
+  font-size: 16px;
+  text-align: center;
+  text-decoration: none;
+  background-color: #4caf50;
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
   cursor: pointer;
-  outline: none;
-  &:hover{
-    opacity: 1;
+  transition: background-color 0.3s, box-shadow 0.3s;
+  &:hover {
+    background-color: #45a049;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
 `;
 
@@ -56,8 +56,8 @@ export const SearchFormInput = styled.input`
   outline: none;
   padding-left: 10px;
   padding-right: 10px;
-  &::placeholder{
+  &::placeholder {
     font: inherit;
     font-size: 18px;
   }
-`
+`;
--- src/pages/Pages.styled.jsx
+++ src/pages/Pages.styled.jsx
@@ -1,42 +1,50 @@
 import styled from 'styled-components';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const SectionStyle = styled.section`
-padding: 20px;
-display: flex;
-flex-direction: column;
-gap: 20px;
-`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
 export const StyledLink = styled(Link)`
-  color: red;
+  color: #ffffff;
+  background-color: #4caf50;
   font-weight: 700;
-  padding: 6px;
-  border: 1px solid red;
-width: 100px;
-  &.active {
-    color: orange;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  width: 100px;
+  transition: background-color 0.3s, box-shadow 0.3s;
+  &:hover {
+    color: #fff;
+    background-color: #45a049;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
 `;
 
 export const WrapStyle = styled.div`
-display: flex;
-gap: 20px;
+  display: flex;
+  gap: 20px;
 `;
 
 export const CardStyle = styled.div`
-display: flex;
-flex-direction: column;
-gap: 20px;
-width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 400px;
 `;
 
 export const AddListStyle = styled.ul`
-display: flex;
-flex-direction: column;
-gap: 10px;
-font-weight: 500;
-text-decoration: underline;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-weight: 500;
+  text-decoration: underline;
 `;
 
 export const FooterStyle = styled.div`
@@ -52,7 +60,7 @@ export const FooterStyle = styled.div`
   padding-top: 12px;
   padding-bottom: 12px;
   color: #fff;
-  background-color: #3f51b5;
+  background-color: #b7cabc;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
