035c8a61db9d6e2e993dd64a2a841ff3832fffa8
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-18 16:28:23 +0100

    update MoviesList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,34 +1,45 @@
-import { Link, useLocation } from "react-router-dom";
-import PropTypes from 'prop-types'
-import { LiStyle, UlStyle, WrapCardStyle } from "./MoviesList.styled";
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { LiStyle, UlStyle, WrapCardStyle } from './MoviesList.styled';
 
 const MoviesList = ({ films }) => {
-    const location = useLocation();
-    
-    return <UlStyle>
-        {films.map(({ id, title, overview, poster_path }) =>
-            <Link to={`/movies/${id}`} state={{ from: location }}>
-                <LiStyle key={id}>
-                    <img src={poster_path ?
-                        `http://image.tmdb.org/t/p/w154${poster_path}` :
-                        'https://www.braasco.com//ASSETS/IMAGES/ITEMS/ZOOM/no_image.jpeg'}
-                        alt={title} width="154" height="231" />
-                    <WrapCardStyle>
-                        <h3>{title}</h3>
-                        <p>{overview}</p>
-                    </WrapCardStyle>
-                </LiStyle>
-            </Link>
-        )}
+  const location = useLocation();
+
+  return (
+    <UlStyle>
+      {films.map(({ id, title, overview, poster_path }) => (
+        <LiStyle key={id}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            <img
+              src={
+                poster_path
+                  ? `http://image.tmdb.org/t/p/w154${poster_path}`
+                  : 'https://www.braasco.com//ASSETS/IMAGES/ITEMS/ZOOM/no_image.jpeg'
+              }
+              alt={title}
+              width="154"
+              height="231"
+            />
+            <WrapCardStyle>
+              <h3>{title}</h3>
+              <p>{overview}</p>
+            </WrapCardStyle>
+          </Link>
+        </LiStyle>
+      ))}
     </UlStyle>
+  );
 };
 
 MoviesList.propTypes = {
-    films: PropTypes.arrayOf(
-        PropTypes.objectOf(
-            PropTypes.node.isRequired,
-        ).isRequired,
-    ).isRequired,
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      overview: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+    }).isRequired
+  ).isRequired,
 };
 
 export default MoviesList;
